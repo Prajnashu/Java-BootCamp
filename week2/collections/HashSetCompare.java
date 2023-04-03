4bf6e80ec31d26460b1122a6d3fbdec7a2201d04
pradehur <pradehur@publicisgroupe.net>
2023-04-03 16:23:32 +0530

    hashSet comparison

--- week2/collections/HashSetCompare.java
+++ week2/collections/HashSetCompare.java
@@ -14,7 +14,21 @@ public class HashSetCompare {
         HashSet<String> setTwo = new HashSet<>();
         setTwo.add("Blue");
         setTwo.add("Black");
+        setTwo.add("Pink");
         setTwo.add("Orange");
         setTwo.add("White");
+
+        boolean areSame = true;
+        for (String str : setOne) {
+            if(!setTwo.contains(str)) {
+                areSame = false;
+            }
+        }
+
+        if(areSame) {
+            System.out.println("Set Two contains set 1");
+        } else {
+            System.out.println("Set two doesn't contain all elements of set 1.");
+        }
     }
 }
