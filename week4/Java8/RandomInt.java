fde186a15b1648e3faa899ea195e3277b20b9a02
pradehur <pradehur@publicisgroupe.net>
2023-04-21 10:26:58 +0530

    RandomIntGenerator.java

---
+++ week4/Java8/RandomInt.java
@@ -0,0 +1,16 @@
+package week4.Java8;
+
+import java.util.Random;
+
+public class RandomInt {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+    for(int i=0; i<30; i++) {
+        if(i%6 == 0) 
+            System.out.println();
+        System.out.println(rand.nextInt(3000, 6000));
+        }
+    }
+    
+}
